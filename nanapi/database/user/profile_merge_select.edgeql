933933e1522bd986e4320679a3c77fc2a6289c8c
voltini <kasssim86@gmail.com>
2024-12-05 20:44:08 +0100

    [profiles] update profile edit query

--- nanapi/database/user/profile_merge_select.edgeql
+++ nanapi/database/user/profile_merge_select.edgeql
@@ -1,9 +1,11 @@
 with
   discord_id := <int64>$discord_id,
   discord_username := <str>$discord_username,
+  birthday := <optional datetime>$birthday,
   full_name := <optional str>$full_name,
   photo := <optional str>$photo,
   promotion := <optional str>$promotion,
+  pronouns := <optional str>$pronouns,
   telephone := <optional str>$telephone,
   user := (
     insert user::User {
@@ -19,26 +21,32 @@ with
   ),
   profile := (
     insert user::Profile {
+      birthday := birthday,
       full_name := full_name,
       photo := photo,
       promotion := promotion,
+      pronouns := pronouns,
       telephone := telephone,
       user := user,
     }
     unless conflict on .user
     else (
       update user::Profile set {
+        birthday := birthday,
         full_name := full_name,
         photo := photo,
         promotion := promotion,
+        pronouns := pronouns,
         telephone := telephone,
       }
     )
   )
 select profile {
+  birthday,
   full_name,
   photo,
   promotion,
+  pronouns,
   telephone,
   user: {
     discord_id,
--- nanapi/database/user/profile_merge_select.py
+++ nanapi/database/user/profile_merge_select.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from edgedb import AsyncIOExecutor
 from pydantic import BaseModel, TypeAdapter
 
@@ -5,9 +7,11 @@ EDGEQL_QUERY = r"""
 with
   discord_id := <int64>$discord_id,
   discord_username := <str>$discord_username,
+  birthday := <optional datetime>$birthday,
   full_name := <optional str>$full_name,
   photo := <optional str>$photo,
   promotion := <optional str>$promotion,
+  pronouns := <optional str>$pronouns,
   telephone := <optional str>$telephone,
   user := (
     insert user::User {
@@ -23,26 +27,32 @@ with
   ),
   profile := (
     insert user::Profile {
+      birthday := birthday,
       full_name := full_name,
       photo := photo,
       promotion := promotion,
+      pronouns := pronouns,
       telephone := telephone,
       user := user,
     }
     unless conflict on .user
     else (
       update user::Profile set {
+        birthday := birthday,
         full_name := full_name,
         photo := photo,
         promotion := promotion,
+        pronouns := pronouns,
         telephone := telephone,
       }
     )
   )
 select profile {
+  birthday,
   full_name,
   photo,
   promotion,
+  pronouns,
   telephone,
   user: {
     discord_id,
@@ -58,9 +68,11 @@ class ProfileMergeSelectResultUser(BaseModel):
 
 
 class ProfileMergeSelectResult(BaseModel):
+    birthday: datetime | None
     full_name: str | None
     photo: str | None
     promotion: str | None
+    pronouns: str | None
     telephone: str | None
     user: ProfileMergeSelectResultUser
 
@@ -73,18 +85,22 @@ async def profile_merge_select(
     *,
     discord_id: int,
     discord_username: str,
+    birthday: datetime | None = None,
     full_name: str | None = None,
     photo: str | None = None,
     promotion: str | None = None,
+    pronouns: str | None = None,
     telephone: str | None = None,
 ) -> ProfileMergeSelectResult:
     resp = await executor.query_single_json(
         EDGEQL_QUERY,
         discord_id=discord_id,
         discord_username=discord_username,
+        birthday=birthday,
         full_name=full_name,
         photo=photo,
         promotion=promotion,
+        pronouns=pronouns,
         telephone=telephone,
     )
     return adapter.validate_json(resp, strict=False)
