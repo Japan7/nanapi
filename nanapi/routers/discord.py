7b753367e7224db46f642c66ca2b443328f29440
NextFire <git@yuru.moe>
2025-05-30 19:18:34 +0200

    [discord] typo
    💀

--- nanapi/routers/discord.py
+++ nanapi/routers/discord.py
@@ -10,13 +10,13 @@ from nanapi.utils.fastapi import NanAPIRouter
 router = NanAPIRouter(prefix='/discord', tags=['discord'])
 
 
-@router.oauth2.put('/messsages/{message_id}', response_model=MessageMergeResult)
+@router.oauth2.put('/messages/{message_id}', response_model=MessageMergeResult)
 async def upsert_message(message_id: str, body: UpsertMessageBody):
     """Create or update a Discord message."""
     return await message_merge(get_edgedb(), message_id=message_id, data=body.data)
 
 
-@router.oauth2.delete('/messsages', response_model=list[MessageBulkDeleteResult])
+@router.oauth2.delete('/messages', response_model=list[MessageBulkDeleteResult])
 async def delete_messages(message_ids: str):
     """Delete Discord messages."""
     return await message_bulk_delete(get_edgedb(), message_ids=message_ids.split(','))
