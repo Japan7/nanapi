805bed9413cde025477497756e303de5d0450670
NextFire <git@yuru.moe>
2025-05-31 12:28:06 +0200

    Revert "[discord] bulk insert route"
    This reverts commit 897be9017ab8f1d2f77e5f343a317401ff505381.

--- nanapi/routers/discord.py
+++ nanapi/routers/discord.py
@@ -1,6 +1,5 @@
 from typing import Annotated, Any
 
-import orjson
 from fastapi import Body, Depends
 from gel import AsyncIOClient
 from pydantic import Json
@@ -9,26 +8,10 @@ from nanapi.database.discord.message_bulk_delete import (
     MessageBulkDeleteResult,
     message_bulk_delete,
 )
-from nanapi.database.discord.message_bulk_insert import (
-    MessageBulkInsertResult,
-    message_bulk_insert,
-)
 from nanapi.database.discord.message_merge import MessageMergeResult, message_merge
 from nanapi.utils.fastapi import NanAPIRouter, get_client_edgedb
 
 router = NanAPIRouter(prefix='/discord', tags=['discord'])
-
-
-@router.oauth2_client_restricted.post('/messages', response_model=list[MessageBulkInsertResult])
-async def bulk_insert_messages(
-    messages: Annotated[list[Json[Any]], Body()],
-    edgedb: AsyncIOClient = Depends(get_client_edgedb),
-):
-    """Bulk create Discord messages."""
-    return await message_bulk_insert(
-        edgedb,
-        messages=[orjson.dumps(data).decode() for data in messages],
-    )
 
 
 @router.oauth2_client_restricted.delete('/messages', response_model=list[MessageBulkDeleteResult])
